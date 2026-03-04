1c9a4fe693c5dd62f6e0d0751a6e3482f37c6a2f
MelnikovAleksandr <MelnikovJavaDev@gmail.com>
2026-03-04 23:01:26 +0500

    feat: splash, icons

--- app/src/main/java/ru/asmelnikov/liquidshopper/presentation/MainActivity.kt
+++ app/src/main/java/ru/asmelnikov/liquidshopper/presentation/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.asmelnikov.liquidshopper.presentation.mainstate.rememberAppState
 import ru.asmelnikov.liquidshopper.presentation.navigation.NavGraph
 import ru.asmelnikov.liquidshopper.presentation.theme.LiquidShopperTheme
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             val appState = rememberAppState()
             LiquidShopperTheme {
